02a0e6c933f55bb2a14ddac27582358167af4002
Erik Dackebro <erik.dackebro@gmail.com>
2018-03-18 12:34:01 +0100

    Add files via upload

---
+++ .gitignore/frmUpload.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace upload_to_temperatur.nu
+{
+    partial class frmSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtDecimaler = new System.Windows.Forms.TextBox();
+            this.tmrUploadInterval = new System.Windows.Forms.Timer(this.components);
+            this.lblDecimal = new System.Windows.Forms.Label();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.txtUploadInterval = new System.Windows.Forms.TextBox();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.txtHash = new System.Windows.Forms.TextBox();
+            this.webUpload = new System.Windows.Forms.WebBrowser();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblLastUploaded = new System.Windows.Forms.Label();
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDecimaler
+            // 
+            this.txtDecimaler.Location = new System.Drawing.Point(126, 6);
+            this.txtDecimaler.Name = "txtDecimaler";
+            this.txtDecimaler.Size = new System.Drawing.Size(35, 20);
+            this.txtDecimaler.TabIndex = 1;
+            this.txtDecimaler.Text = "1";
+            this.txtDecimaler.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDecimaler_KeyPress);
+            // 
+            // tmrUploadInterval
+            // 
+            this.tmrUploadInterval.Tick += new System.EventHandler(this.tmrUploadInterval_Tick);
+            // 
+            // lblDecimal
+            // 
+            this.lblDecimal.AutoSize = true;
+            this.lblDecimal.Location = new System.Drawing.Point(38, 9);
+            this.lblDecimal.Name = "lblDecimal";
+            this.lblDecimal.Size = new System.Drawing.Size(82, 13);
+            this.lblDecimal.TabIndex = 2;
+            this.lblDecimal.Text = "Antal decimaler:";
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(73, 35);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(47, 13);
+            this.lblDirectory.TabIndex = 4;
+            this.lblDirectory.Text = "Sökväg:";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(126, 32);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(208, 20);
+            this.txtDirectory.TabIndex = 3;
+            this.txtDirectory.Text = "F:\\Temperatur.nu\\Värden\\";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(71, 61);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(49, 13);
+            this.lblFileName.TabIndex = 6;
+            this.lblFileName.Text = "Fil namn:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(126, 58);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(208, 20);
+            this.txtFileName.TabIndex = 5;
+            this.txtFileName.Text = "last.htm";
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(12, 87);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(108, 13);
+            this.lblInterval.TabIndex = 8;
+            this.lblInterval.Text = "Uppladdings intervall:";
+            // 
+            // txtUploadInterval
+            // 
+            this.txtUploadInterval.Location = new System.Drawing.Point(126, 84);
+            this.txtUploadInterval.Name = "txtUploadInterval";
+            this.txtUploadInterval.Size = new System.Drawing.Size(208, 20);
+            this.txtUploadInterval.TabIndex = 7;
+            this.txtUploadInterval.Text = "60";
+            // 
+            // lblHash
+            // 
+            this.lblHash.AutoSize = true;
+            this.lblHash.Location = new System.Drawing.Point(67, 113);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(53, 13);
+            this.lblHash.TabIndex = 10;
+            this.lblHash.Text = "Hashkod:";
+            // 
+            // txtHash
+            // 
+            this.txtHash.Location = new System.Drawing.Point(126, 110);
+            this.txtHash.Name = "txtHash";
+            this.txtHash.Size = new System.Drawing.Size(208, 20);
+            this.txtHash.TabIndex = 9;
+            this.txtHash.Text = "0c4e88a2b432146501c63d9182d9aaf9";
+            // 
+            // webUpload
+            // 
+            this.webUpload.Location = new System.Drawing.Point(12, 12);
+            this.webUpload.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webUpload.Name = "webUpload";
+            this.webUpload.Size = new System.Drawing.Size(250, 114);
+            this.webUpload.TabIndex = 11;
+            this.webUpload.Visible = false;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(45, 136);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 12;
+            this.btnApply.Text = "Tillämpa";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(126, 136);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblLastUploaded
+            // 
+            this.lblLastUploaded.AutoSize = true;
+            this.lblLastUploaded.Location = new System.Drawing.Point(12, 162);
+            this.lblLastUploaded.Name = "lblLastUploaded";
+            this.lblLastUploaded.Size = new System.Drawing.Size(0, 13);
+            this.lblLastUploaded.TabIndex = 14;
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Location = new System.Drawing.Point(258, 189);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(75, 23);
+            this.btnTest1.TabIndex = 15;
+            this.btnTest1.Text = "Test1";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click_1);
+            // 
+            // frmSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 304);
+            this.Controls.Add(this.btnTest1);
+            this.Controls.Add(this.lblLastUploaded);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.txtHash);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.txtUploadInterval);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.lblDecimal);
+            this.Controls.Add(this.txtDecimaler);
+            this.Controls.Add(this.webUpload);
+            this.Name = "frmSettings";
+            this.Text = "Uploading data to temperatur.nu";
+            this.Load += new System.EventHandler(this.frmSettings_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDecimaler;
+        private System.Windows.Forms.Timer tmrUploadInterval;
+        private System.Windows.Forms.Label lblDecimal;
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.TextBox txtUploadInterval;
+        private System.Windows.Forms.Label lblHash;
+        private System.Windows.Forms.TextBox txtHash;
+        private System.Windows.Forms.WebBrowser webUpload;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblLastUploaded;
+        private System.Windows.Forms.Button btnTest1;
+    }
+}
+
---
+++ .gitignore/frmUpload.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace upload_to_temperatur.nu
+{
+    public partial class frmSettings : Form
+    {
+        public frmSettings()
+        {
+            InitializeComponent();
+        }
+
+
+
+        decimal temperature1 = 0; ///Temperaturen för mätare 1 i decimalform
+        decimal temperature2 = 0; ///Temperaturen för mätare 2 i decimalform
+        string directory = ""; ///Directory som .htm filen ligger i
+        string file = ""; ///.htm filens namn
+        string hash = ""; ///sin egen hashkod
+        int antalDecimaler = 0; ///Antal decimaler i mätt värde
+        int uploadInterval = 0; ///Uppladdnings intervall för timern
+        string fullUrl = ""; ///url för browsern
+
+
+        ///
+        ///Hämtar temperaturen för mätare 
+        ///
+        public decimal getTemperatur(int line)
+            {
+                string data = ""; ///Hela raden med temperaturen
+                string value = "";///Värdet för mätaren i textform
+                
+                using(StreamReader read = new StreamReader( (directory+file).ToString()) ) ///Streamreader läser data från filen, stängs automatiskt
+                    {
+
+                        for (int i =0; i <= line; i++) ///Loop som läser varje rad fram till raden som angets (där värdet finns). Går sedan ur loopen och kvar finns temperaturen och resten av texten på raden.
+                            {
+                                data = read.ReadLine();///Läs värdet på raden
+                            }//for
+
+                        Console.WriteLine(data); ///Skriv på outputraden
+                        
+                        data = data.Remove(0, 19); ///Ta bort de 19 första tecknen på raden (endast HTML kod)
+                        Console.WriteLine(data); ///Skriv på outputraden
+                        
+                        int antalTecken = 0; ///Ny variabel, ska hålla antal tecken för temperaturen inklusive decimaler och decimaltecknet
+                        antalTecken = data.IndexOf(","); ///Ta reda på hur många tecken (siffor) det kommer innan kommatecknet och spara värdet i variabeln
+                        antalTecken = antalTecken + 1 + antalDecimaler; ///Lägg på 1 för kommatecknet och det antal decimaler man anget finns med i det mätta värdet
+                        
+                        value = data.Remove(antalTecken, 5); ///Ta bort restrerande HTML kod efter de antal tecken som nu tagits reda på
+
+                        Console.WriteLine(value); ///Skriv på outputraden
+
+                        return Convert.ToDecimal(value); ///Returnera värdet konveterat till ett tal i decimalform
+
+                    }//using
+            }//getTemperatur628
+
+        private void applySettings() ///Tillämpa förändringar
+            {
+                antalDecimaler = Convert.ToInt32(txtDecimaler.Text); ///Antal decimaler konverterat till integer
+
+                file = txtFileName.Text;///Filens namn
+
+                hash = txtHash.Text; ///Hashkoden
+
+                directory = txtDirectory.Text; ///Katalogen
+                directory = directory.Replace("\\", "\\\\"); ///Ersätt ett backslash med två för tillämpling vid sökvägar
+
+                uploadInterval = Convert.ToInt32(txtUploadInterval.Text); ///Timerns intervall i sekunder
+                uploadInterval = uploadInterval * 1000; ///Omvandla till millisekunder
+                tmrUploadInterval.Interval = uploadInterval; ///Spara intervallet
+                
+            }//applySettings
+
+
+
+
+        private void btnTest1_Click(object sender, EventArgs e)
+            {
+                /*
+                temperature1 = getTemperatur(16); ///Värdet för mätare 1 ligger på rad 16
+
+                Console.WriteLine("Följande är värdet i decimalform: " + temperature1.ToString() + "\n\n\n"); ///Skriv på outputraden
+
+                temperature2 = getTemperatur(11); ///Värdet för mätare 2 ligger på rad 11
+
+                Console.WriteLine("Följande är värdet i decimalform: " + temperature2.ToString() + "\n\n\n"); ///Skriv på outputraden
+                */
+
+                tmrUploadInterval_Tick(null, null);
+                
+                }//btnTest1_Click
+
+
+
+
+        private void frmSettings_Load(object sender, EventArgs e)
+            {
+                ///Här ska väderna laddas från fil i framtiden
+
+                applySettings();
+                
+            }
+
+
+        private void txtDecimaler_KeyPress(object sender, KeyPressEventArgs e) ///Allt förutom siffertangenter blockeras
+            {
+                if (!char.IsDigit(e.KeyChar)) ///Ta reda på om den tryckta tangenten är en siffra
+                    {
+                        e.Handled = true; ///I sådana fall accepteras värdet
+                    }//if
+            }//txtDecimaler_KeyPress
+
+        private void tmrUploadInterval_Tick(object sender, EventArgs e)
+            {
+                decimal lowValue = 0; ///Lägsta uppmätta värde
+                
+                temperature2 = getTemperatur(13); ///Värdet för mätare 1 ligger på rad 11
+                temperature1 = getTemperatur(18); ///Värdet för mätare 2 ligger på rad 16
+                
+                if (temperature1 >= temperature2) ///Om temperatur1 är mer än eller lika med temperatur2
+                    {
+                        lowValue = temperature2; ///Är temperatur2 det lägsta värdet
+                    }// if
+                else
+                    {
+                        lowValue = temperature1; ///Annars är temperatur1 det lägsta värdet
+                    }//else
+
+                fullUrl = "http://www.temperatur.nu/rapportera.php?hash=" + hash + "&t=" + Convert.ToString(lowValue); ///Ta fram url
+                
+                try ///Försök navigera till url 
+                    {
+                        webUpload.Navigate(new Uri(fullUrl));
+                        lblLastUploaded.Text = "Senast uppladdat: " + System.DateTime.UtcNow.ToString() + "\nMed temperaturen: " + lowValue.ToString();
+                    }//try
+                catch (System.UriFormatException)
+                    {
+                        return;
+                    }//catch
+
+                tmrUploadInterval.Enabled = true;
+
+            }//tmrUploadInterval_Tick
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            applySettings();
+
+            Console.WriteLine(directory + file + "\nUpload interval: " + uploadInterval.ToString() + "\nDecimaler: " + antalDecimaler.ToString() + "\nHash: " + hash);
+        }//btnApply_Click
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrUploadInterval.Enabled = true;
+        }
+
+        private void btnTest1_Click_1(object sender, EventArgs e)
+        {
+            tmrUploadInterval_Tick(null, null);
+        }//btnStart_Click
+
+
+
+    }
+}
